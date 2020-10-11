a3b8f3e5c4070f1016885c003d25526196c6a63e
DylanLADERRIERE <mrdydymmaxx@gmail.com>
2020-10-11 19:56:17 +0200

    feat(exercice2): create drawBar function

--- exercice-2/script.js
+++ exercice-2/script.js
@@ -5,4 +5,14 @@ var Bar = function constructor(sum,nbr) {
  this.sum = sum;
  this.nbr = nbr;
 }
+
+Bar.prototype.drawBar = function () {
+	var progress = document.createElement('progress');
+	progress.max = this.sum;
+	progress.value = this.nbr;
+	progress.style.width = '100%';
+	document.body.appendChild(progress);
+}
+
 var bar = new Bar(100,79);
+bar.generate();
