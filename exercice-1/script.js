97ba47ad93cb8039b992ecc15e38266cbeb35f46
DylanLADERRIERE <mrdydymmaxx@gmail.com>
2020-10-11 15:01:02 +0200

    feat(exercice1): generate table

--- exercice-1/script.js
+++ exercice-1/script.js
@@ -11,6 +11,20 @@ var GridGenerator = function constructor(xAxis,yAxis) {
  this.yAxis = yAxis;
 }
 
-var grid = new GridGenerator(5,7);
-console.log(grid.xAxis);
-console.log(grid.yAxis);
+GridGenerator.prototype.generate = function () {
+		var table = document.createElement('table');
+		for (var i = 0; i < this.xAxis; i++) {
+			var tr = document.createElement('tr');
+			table.appendChild(tr);
+			for (var j = 0; j < this.yAxis; j++) {
+				var td = document.createElement('td');
+				td.width='25px';
+				td.height='25px';
+				td.style.border='1px solid black';
+				tr.appendChild(td);
+		}
+	}
+	document.body.appendChild(table);
+}
+var grid = new GridGenerator(10,10);
+grid.generate();
