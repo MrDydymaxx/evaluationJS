44b51dac48cb54124361c79f323e9c298dc111e5
DylanLADERRIERE <mrdydymmaxx@gmail.com>
2020-10-14 23:31:50 +0200

    doc(exercice9): add jsdoc for exercice 9 functions

--- exercice-9/script.js
+++ exercice-9/script.js
@@ -1,5 +1,11 @@
 "use strict";
 
+/**
+ * @checkPalindrome
+ * @param {String} palindrome - check if the is a palindrome 
+ * @return {boolean} - false if it's not a palindrome, true if it's a palindrome
+ */
+
 function checkPalindrome(palindrome) {
 	for (var i = 0; i < palindrome.length/2; i++) {
    if (palindrome[i] !== palindrome[palindrome.length - 1 - i]) {
