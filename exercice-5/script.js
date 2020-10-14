3341e9374586d3dc4f6a7bd9b9fafb258a3312fb
DylanLADERRIERE <mrdydymmaxx@gmail.com>
2020-10-14 10:04:38 +0200

    feat(exercice5): check form function

--- exercice-5/script.js
+++ exercice-5/script.js
@@ -25,11 +25,45 @@ var MyForm = function() {
 			input.type ='button';
 			input.id='send';
 			input.value ='Envoyer';
+			input.addEventListener('click',function(){
+				alert(checkForm());
+			});
 		}
 		form.appendChild(input);
 		var br = document.createElement('br');
 		form.appendChild(br);
 	}
 }
+function checkForm() {
+	const regexName = new RegExp('[^0-9\\.\\,\\"\\?\\!\\;\\:\\#\\$\\%\\&\\(\\)\\*\\+\\-\\/\\<\\>\\=\\@\\[\\]\\\\^\\_\\{\\}\\|\\~]+');
+	const regexMail = new RegExp('\\b[\\w\\.-]+@[\\w\\.-]+\\.\\w{2,4}\\b','gi');
+	const regexPass = new RegExp('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$','gm');
+	var error;
+	var input = document.getElementById('name').value;
+	if (!regexName.test(input)) {
+		error = "Name not valid!";
+		return error;
+	}
+	input = document.getElementById('lastName').value;
+	if (!regexName.test(input)) {
+		error = "Last name not valid!";
+		return error;
+	}
+	input = document.getElementById('email').value;
+	if (!regexMail.test(input)) {
+		error = "Mail not valid!";
+		return error;
+	}
+	input = document.getElementById('password').value;
+	if (!regexPass.test(input)) {
+		error = "Password not valid! \nPlease make sur to use at least :\nOne uppercase character\nOne lowercase character\nOne number\n8 character long";
+		return error;
+	}
+	if(error == undefined) {
+		error = 'All informations are valid!';
+		return error;
+	}
+
+};
 
 var formulaire = new MyForm();
