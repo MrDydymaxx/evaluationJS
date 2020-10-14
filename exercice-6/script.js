1eafb3ed2c039cb6059960d2fddaa66a7e1e6231
DylanLADERRIERE <mrdydymmaxx@gmail.com>
2020-10-14 14:45:46 +0200

    feat(exercice6): add checkPhoneNumber function

--- exercice-6/script.js
+++ exercice-6/script.js
@@ -14,10 +14,28 @@ var MyPhoneNumber = function () {
 			input.type ='button';
 			input.id='send';
 			input.value ='Envoyer';
+			input.addEventListener('click',function(){
+				alert(checkPhoneNumber());
+			});
 		}
 		form.appendChild(input);
 		var br = document.createElement('br');
 		form.appendChild(br);
 	}
+}
+function checkPhoneNumber() {
+	const regexPhone = new RegExp("(\\(\\+33\\)|0|\\+33|0033)[1-9]([0-9]{8}|([0-9\\- ]){12})",'g');
+	var error;
+	var input = document.getElementById('number').value;
+	if (!regexPhone.test(input)) {
+		error = "Number not valid!";
+		return error;
+	}
+	var checkNumberType = input.substring(0,2);
+	if (checkNumberType == '01' || checkNumberType == '06' || checkNumberType == '07') {
+		return 'This phone number is a 06, 07 or 01 : true';
+	}
+	return 'This is a valid phone number!'
+
 }
 var phone = new MyPhoneNumber();
