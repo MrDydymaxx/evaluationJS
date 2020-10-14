591252ca4432a22309aef74e888e27609696fad0
DylanLADERRIERE <mrdydymmaxx@gmail.com>
2020-10-14 10:10:41 +0200

    feat(exercice3): add checkWin function

--- exercice-3/script.js
+++ exercice-3/script.js
@@ -35,8 +35,48 @@ var MyMorpionXO = function() {
 				td.appendChild(img);
 				td.setAttribute('value',value);
 				}
+				checkWin();
 			});
 			tr.appendChild(td);
+		}
+	}
+}
+function checkWin() {
+	console.log('ok');
+	var winConditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+	var player1 = [], player2 = [];
+	var player1win = 0, player2win = 0;
+	for (var i = 0; i < 9; i++) {
+		var element = document.getElementById(i);
+		if(element.getAttribute('value') == 1) {
+			player1.push(i);
+		}
+		if (element.getAttribute('value') == 2) {
+			player2.push(i);
+		}
+		for (var j = 0; j < winConditions.length; j++) {
+			for (var k = 0; k < winConditions[j].length; k++) {
+				for (var l = 0; l < player1.length; l++) {
+					if(winConditions[j][k] == player1[l]) {
+						player1win++;
+						if (player1win == 3) {
+							alert("player 1 win");
+							return this.player1score++;
+						}
+					}
+				}
+				for (var l = 0; l < player2.length; l++) {
+					if(winConditions[j][k] == player2[l]) {
+						player2win++;
+						if (player2win == 3) {
+							alert("player 2 win");
+							return this.player2score++;
+						}
+					}
+				}
+			}
+			player1win = 0;
+			player2win = 0;
 		}
 	}
 }
